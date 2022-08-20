ebe80e36f097819a694081861d08e607194f8db4
tamilaiakobadze <110965352+tamilaiakobadze@users.noreply.github.com>
2022-08-20 15:37:27 +0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+x = float(input("enter x:"))
+
+oper = input("select an operation: +, -, *, /, ^---> ")
+
+y = float(input(" enter y: " ))
+
+if oper == " + "
+      print( x + y )
+elif oper = = "- ":
+       print( x - y )
+elif oper = = " / ":
+       if y !=0;
+           print( x / y )
+elif oper = = " * ":
+       print( x * y )
+elif oper = = " ^ ":
+       print( x ** y )
+
+else:
+    print( " the selected operation is incorrect! " )
